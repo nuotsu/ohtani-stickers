2dc4d1aa6f0a163fbba29f369abbf7fc878aa23a
Mitchell Christ <xzmaru@gmail.com>
2021-10-23 20:56:29 -0700

    style header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,3 +1,15 @@
-<header class="py-4 px-2 text-center text-2xl font-serif font-bold">
-	<h1>The Faces of <span class="inline-block">Shohei Ohtani</span></h1>
+<header class="py-8 px-2 text-center font-serif font-bold">
+	<h1 class="max-w-max mx-auto">
+		The Faces of
+		<strong class="relative block text-2xl">
+			Shohei Ohtani
+			<span>17</span>
+		</strong>
+	</h1>
 </header>
+
+<style lang="postcss">
+	span {
+		@apply absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-2/3 text-red text-opacity-10 text-6xl leading-none;
+	}
+</style>
