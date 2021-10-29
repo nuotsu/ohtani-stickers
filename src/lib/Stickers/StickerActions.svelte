34cdf59a61419e0151fce54070a7f332566e3ede
Mitchell Christ <xzmaru@gmail.com>
2021-10-28 19:34:48 -0700

    add img blob fetch

--- src/lib/Stickers/StickerActions.svelte
+++ src/lib/Stickers/StickerActions.svelte
@@ -1,12 +1,12 @@
 <p class="flex flex-wrap justify-evenly gap-2 mt-2">
-	{#if navigator.canShare}
-		<button class="action">
+	{#if navigator.canShare && navigator.canShare(shareData())}
+		<button class="action" on:click={() => navigator.share(shareData())}>
 			<IconShare/> Share
 		</button>
 	{:else}
 		<a href={downloadUrl} class="action">
 			<IconDownload/> Download
-		</a>`
+		</a>
 	{/if}
 
 	<a href={originalUrl} {...newtab} class="action">
@@ -27,9 +27,22 @@
 
 	export let image, emotion
 
+	let filename = `shohei-ohtani-${emotion.emoji}`
+
+	$: async () => {
+		let u = await fetch(downloadUrl)
+		console.log({ u })
+	}
+
+	async function shareData() {
+		let blob = (await fetch(downloadUrl)).blob()
+		let file = new File([blob], filename, {type: blob.type})
+		return {files: [file]}
+	}
+
 	$: originalUrl = image.source || image.original
 
 	$: downloadUrl = urlFor(image.face)
-		.forceDownload(`shohei-ohtani-${emotion.emoji}`)
+		.forceDownload(filename)
 		.url()
 </script>
